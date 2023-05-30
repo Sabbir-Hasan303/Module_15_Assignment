3422f60cad58e291d2eac057f1863f7da108432a
Sabbir-Hasan303 <64860849+Sabbir-Hasan303@users.noreply.github.com>
2023-05-30 21:38:23 +0600

    Update footer.blade.php

--- resources/views/Layout/footer.blade.php
+++ resources/views/Layout/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="py-5">
     <div class="container">
-        <p class="text-center">All rights reserved © MD TAYOUBUR RAHMAN 2023</p>
+        <p class="text-center">All rights reserved © Syed Sabbir Hasan</p>
     </div>
 </footer>
